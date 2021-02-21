8710aba3fbe62919ec59e79fd66301507d6a4861
Julio Labarca <jlabarca25@gmail.com>
2021-02-20 22:54:08 -0300

    Tiles Pooling

---
+++ Assets/Scripts/Core/Pooling/TilesPool.cs
@@ -0,0 +1,15 @@
+﻿using Jlabarca.OneBigMob.MonoBehaviours;
+using Leopotam.Ecs;
+
+namespace Jlabarca.OneBigMob.Core.Pooling
+{
+    public class TilesPool : GenericObjectPool<TileView>
+    {
+        public override void ReturnToPool(TileView objectToReturn)
+        {
+            var entity = objectToReturn.Entity;
+            entity.Del<Core.Unit.Components.Unit>();
+            base.ReturnToPool(objectToReturn);
+        }
+    }
+}
